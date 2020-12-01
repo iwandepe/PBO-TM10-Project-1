6200e2ce9ae0f03528f693df06d6543b383baa93
Iwan Dwi Prakoso <iwan.dprakoso@gmail.com>
2020-12-01 11:09:23 +0700

    Inisialisasi Project

---
+++ src/id/ac/its/kelompok/MainApp.java
@@ -0,0 +1,15 @@
+package id.ac.its.kelompok;
+
+import javax.swing.JOptionPane;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// obtain user input from JOptionPane input dialogs
+		
+		
+		
+	}
+
+}
